a47df8e58e8ae6c74de82afbd3ea44bf5b02fd1c
Murtaza Naeem <murtaza.naeem23.15@gmail.com>
2026-05-06 15:44:57 +0500

    Update

--- students/Murtaza_Naeem_Batch_05/sql/Joins.sql
+++ students/Murtaza_Naeem_Batch_05/sql/Joins.sql
@@ -41,5 +41,39 @@ INNER JOIN hr.employees  e
 SELECT c.id as candid_id, e.id as emp_id, c.fullname FROM hr.candidates c
 INNER JOIN hr.employees  e
 	ON e.fullname = c.fullname;*/
+-- INNER JOIN
+	select 
+		p.product_id,
+		oi.order_id
+	from production.products p
+	inner JOIN SALES.order_items oi
+	on p.product_id = oi.product_id;
 
-	select * from production.products 
+--LEFT JOIN
+
+SELECT p.product_name, oi.order_id, oi.item_id from production.products p
+left join sales.order_items oi
+on oi.product_id  = p.product_id
+order by oi.order_id;
+
+--RIGHT JOIN
+
+SELECT product_name, order_id from sales.order_items oi
+right join production.products p
+on p.product_id = oi.product_id
+order by order_id;
+
+select * from sales.stores o
+right join sales.orders st
+on st.store_id = o.store_id
+order by st.store_id;
+
+-- cross join
+
+Select * from production.products;
+select * from sales.stores;
+
+select * from production.products
+cross join sales.order_items;
+
+-- self join
